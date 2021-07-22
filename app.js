432cc46288c824051d9cea65ff618f9e9b7f3e0d
Karimouloud <karimachoui@gmx.fr>
2021-07-22 18:34:01 +0200

    schema sauce

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose')
-const saucesRoutes = require('./routes/sauce')
+
 const userRoutes = require('./routes/user');
 
 require('dotenv').config()
@@ -19,15 +19,11 @@ mongoose.connect(process.env.MONGODB,
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 
-    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 
-    'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
 
-app.use((req, res) => {
-   res.json({ message: 'Votre requête a bien été reçue !' }); 
-});
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
