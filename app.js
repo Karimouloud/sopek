d42700e773f127748460f269bb277df099c41d09
Karimouloud <karimachoui@gmx.fr>
2021-07-22 16:29:22 +0200

    mongo

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require('express');
 
+const mongoose = require('mongoose')
+
+require('dotenv').config()
+
 const app = express();
 
 app.use(express.json())
 
+mongoose.connect(process.env.MONGODB,
+    { useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
 
