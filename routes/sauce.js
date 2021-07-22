7f0cee8b02658d35d266fa5b9f6717e0c2b15365
Karimouloud <karimachoui@gmx.fr>
2021-07-22 18:39:03 +0200

    route sauce

---
+++ routes/sauce.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router()
+
+const auth = require('../middleware/auth')
+const multer = require('../middleware/multer-config')
+
+const saucesCtrl = require('../controllers/sauce');
+
+router.post('/', auth, multer, saucesCtrl.createSauce);  
+router.put('/:id', auth, saucesCtrl.modifySauce);  
+router.delete('/:id', auth, saucesCtrl.deleteSauce);  
+router.get('/:id', auth, saucesCtrl.getOneSauce);  
+router.get('/', auth, saucesCtrl.getAllSauces);
+
+module.exports = router;
